3805666223a03f20515ac3b19e9c2e78d08b838f
Mawutor12575 <146585179+Mawutor12575@users.noreply.github.com>
2026-03-26 20:30:16 +0000

    Delete deriv-hedge-bot/api directory

--- deriv-hedge-bot/api/websocket.js
+++
@@ -1,7 +0,0 @@
-const WebSocket = require('ws');
-
-module.exports = async (req, res) => {
-    // This is a placeholder for WebSocket upgrade handling
-    // In production, you'd need to handle WebSocket connections differently
-    res.json({ status: 'WebSocket endpoint ready' });
-};
