730343d7cf7ae6476d72dbbe01ea60106d6f7996
Mawutor12575 <146585179+Mawutor12575@users.noreply.github.com>
2026-03-26 21:30:41 +0000

    Delete deriv-hedge-bot directory

--- deriv-hedge-bot/api/websocket.js
+++
@@ -1,6 +0,0 @@
-module.exports = async (req, res) => {
-    res.json({ 
-        status: 'ready',
-        message: 'WebSocket endpoint. Connect to wss://ws.deriv.com/websockets/v3 directly from frontend.'
-    });
-};
